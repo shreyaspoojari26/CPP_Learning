c07baba9506e60e348b8cbcf617db0190f4ac97f
Shreyas Poojari <shreyaspoojari900@gmail.com>
2026-09-07 21:52:17 +0530

    Implement function overloading in Calculator class

---
+++ Polymorphism/function_overloading.cpp
@@ -0,0 +1,22 @@
+#include <iostream>
+using namespace std;
+
+class Calculator {
+public:
+    int add(int a, int b) {
+        return a + b;
+    }
+
+    int add(int a, int b, int c) {
+        return a + b + c;
+    }
+};
+
+int main() {
+    Calculator c;
+
+    cout << c.add(10, 20) << endl;
+    cout << c.add(10, 20, 30);
+
+    return 0;
+}
